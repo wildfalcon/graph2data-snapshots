2a670164bd16e1b97674b5798589fee7fb2cce06
Laurie Young <laurie@wildfalcon.com>
2019-12-03 16:21:32 +0000

    Sept 30

---
+++ Graph2Data2/precache-manifest.53b2e52357b3130f1c8e583a9b111e3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e027c03cc516d25f4603b0a295644686",
+    "url": "/Graph2Data2/index.html"
+  },
+  {
+    "revision": "ee9963b6996a88dad34f",
+    "url": "/Graph2Data2/static/css/main.9e1a5549.chunk.css"
+  },
+  {
+    "revision": "1821d6aff2f5fd69b9e2",
+    "url": "/Graph2Data2/static/js/2.8531f90b.chunk.js"
+  },
+  {
+    "revision": "ee9963b6996a88dad34f",
+    "url": "/Graph2Data2/static/js/main.1cd41313.chunk.js"
+  },
+  {
+    "revision": "df7e7d4b793afa8d27cf",
+    "url": "/Graph2Data2/static/js/runtime~main.f20b4199.js"
+  }
+]);
